8f3c11fda193ab66789b88f13562bde1e2bb7ea0
Robim-rur <cruvinelebook@gmail.com>
2026-01-13 13:57:09 -0300

    Update gerar_senha.py

--- gerar_senha.py
+++ gerar_senha.py
@@ -2,13 +2,10 @@ from datetime import datetime
 import random
 import string
 
-PREFIXO = "CRUVI"
-
-def gerar_senha():
+def gerar():
     hoje = datetime.now().strftime("%Y-%m-%d")
     codigo = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
-    return f"{PREFIXO}-{hoje}-{codigo}"
+    print(f"CRUVI-{hoje}-{codigo}")
 
 if __name__ == "__main__":
-    print("\nSENHA GERADA:")
-    print(gerar_senha())
+    gerar()
